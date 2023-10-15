2d71b6465ab9aa649cb35f18a361521354404014
zonyxe <thichakorn.ticha@mail.kmutt.ac.th>
2023-10-15 18:16:32 +0700

    SoundSetting+Pause

--- TualengGameJam/Assets/GameJamFile/Script/GameManagement/ButtonManager.cs
+++ TualengGameJam/Assets/GameJamFile/Script/GameManagement/ButtonManager.cs
@@ -3,42 +3,74 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEngine.AudioSettings;
 
 public class ButtonManager : MonoBehaviour
 {
     public static ButtonManager instance;
-    public void Start()
+    public GameObject sliderbgm,menu,settingpopup;
+    private void Awake()
     {
         instance = this;
     }
-
+    
     public void OnClick_Setup()
     {
         GameObject.Find("Startgame_Button").GetComponent<Button>().onClick.AddListener(Onplay);
         GameObject.Find("Setting_Button").GetComponent<Button>().onClick.AddListener(Onsetting);
         GameObject.Find("Exit_Button").GetComponent<Button>().onClick.AddListener(Onexit);
+        GameObject.Find("Setting_Close").GetComponent<Button>().onClick.AddListener(Popupsetting);
+        OC_UI();
 
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mockGameover();
+        }
+    }
+
+    #region Open/Close UI
+
+    void OC_UI()
+    {
+        menu = GameObject.Find("Menu");
+        sliderbgm = GameObject.Find("bgm_Slider");
+        settingpopup = GameObject.Find("Setting_Popup");
+        settingpopup.SetActive(false);
+    }
+
+    public void mockGameover()
+    {
+        menu.SetActive(true);
+        GameManager._gamemanager.start = false;
+        Time.timeScale = 0f;
+    }
+
+    #endregion
 
     #region ButtonFunction
+
 
     void Onplay()
     {
         Debug.Log("Startgame");
-        //close menu ui
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        GameManager._gamemanager.start = true;
+        
     }
-
-    private Vector3 originalSize;
     void Onsetting()
     {
-        Debug.Log("Setting");
-        //Transform place = GameObject.Find("Canvas").GetComponent<RectTransform>().transform;
-        //GameObject popup = (GameObject)Instantiate(Resources.Load("UI/Setting_Popup"));
-        //originalSize = popup.transform.localScale;
-        //popup.transform.parent = place.transform;
-        //popup.transform.localScale = originalSize;
+        Debug.Log("Open Setting");
+        settingpopup.SetActive(true);
+    }
+
+    void Popupsetting()
+    {
+        Debug.Log("Close Setting");
+        settingpopup.SetActive(false);
     }
 
     void Onexit()
--- TualengGameJam/Assets/GameJamFile/Script/GameManagement/GameManager.cs
+++ TualengGameJam/Assets/GameJamFile/Script/GameManagement/GameManager.cs
@@ -5,29 +5,55 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager _gamemanager;
+    public bool start = false;
     public void Start()
     {
+        _gamemanager = this;
         Startgame();
     }
 
+    private void Update()
+    {
+        Pausegame();
+    }
     public void Startgame()
     {
         ButtonManager.instance.OnClick_Setup();
     }
 
-    void Gameover()
+    public void Gameover()
     {
-
+        ButtonManager.instance.mockGameover();
     }
 
     void Pausegame()
     {
-
+        if(start == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale != 0f)
+            {
+                Debug.Log("Pause");
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Debug.Log("Continue");
+                Time.timeScale = 1f;
+            }
+        }
     }
 
     void Continuegame()
     {
-
+        if (start == true && Time.timeScale != 1f)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Continue");
+                Time.timeScale = 1f;
+            }
+        }
     }
 
     void Restartgame()
